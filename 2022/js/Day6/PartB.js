3c2a465835f99762e9c3fd9e5e8def281990a538
ke-green <k.erikgreen@gmail.com>
2024-04-01 17:56:53 -0400

    commit

--- 2022/js/Day6/PartB.js
+++ 2022/js/Day6/PartB.js
@@ -5,11 +5,11 @@ const { open } = require('node:fs/promises');
 (async () => {
 
     const file = await open('./INPUT.TXT');
-    let l = 4;
+    let l = 14;
 
     for await (const line of file.readLines()) {
        
-        for (let i = 0; i < line.length; i++) {    
+        for (let i = 0; i < line.length; i++) {
             let s = line.substring(i, i + l);
             if (!Array.from(s).some((c, i) => s.includes(c, i + 1))) {
                 console.log(`i = ${i}, i+l = ${i + l}, chars = ${s}`);
