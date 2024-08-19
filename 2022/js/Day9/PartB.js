e0f9710a882b7675ce8e2751655a48604791cb36
egreen <egreen@normannoble.net>
2024-08-19 08:13:56 -0400

    sync

--- 2022/js/Day9/PartB.js
+++ 2022/js/Day9/PartB.js
@@ -13,20 +13,18 @@ const { open } = require('node:fs/promises');
         moves.push(move);    
     }
     
-    const knots = Array(10).fill({x : 0, y: 0});
-    
-    knots[0] = {x : 0, y : 0};
+    const knots = Array.from({ length: 10}, (_, i) => ({x : 11, y : 5}));
     let tail = {x : 0, y : 0};
     let i = 0;
 
-    let position = JSON.stringify({x : 0, y : 0});
+   
+    let position = JSON.stringify({x : knots[0].x, y : knots[0].y});
     positions.add(position);
     
     for (let move = 0; move < moves.length; move++) {
         
         let direction = moves[move].split(' ')[0].trim();
         let steps = parseInt(moves[move].split(' ')[1].trim());
-
 
         for (let step = 0; step < steps; step++) {
 
@@ -44,6 +42,7 @@ const { open } = require('node:fs/promises');
                     knots[0].x += 1;
                     break;
             }
+
             
             for (let knot = 1; knot < 10; knot++) {
             
@@ -51,28 +50,46 @@ const { open } = require('node:fs/promises');
                 let xd = (knots[knot - 1].x - knots[knot].x);
                 let moved = false;
 
-                if (Math.abs(yd) > 1) {
+                
+                if (Math.abs(xd) > 1 && Math.abs(yd) > 1) {
+                    knots[knot].x += xd > 0 ? 1 : -1;
+                    knots[knot].y += yd > 0 ? 1 : -1;    
+                }
+                else if (Math.abs(yd) > 1) {
                     knots[knot].y += yd > 0 ? 1 : -1;
                     knots[knot].x = knots[knot - 1].x;
-                    console.log(`yd...${knot}`);
                     moved = true;
-                    
                 }
                 else if (Math.abs(xd) > 1) {
                     knots[knot].x += xd > 0 ? 1 : -1;
                     knots[knot].y = knots[knot - 1].y;
-                    console.log(`xd...${knot}`);
                     moved = true;
                 } 
-
-                if (knot == 9) {
-                    //console.log(`move(${move}): xd-${xd}, yd-${yd}, ${moves[move]}, knot[0]: ${JSON.stringify(knots[0])}}, knot[${knot}]:  ${JSON.stringify(knots[knot])}, mvd: ${moved.toString()}`)
+                
+                /*
+                if (Math.abs(xd) > 1 && Math.abs(yd) > 1) {
+                    knots[knot].x += xd > 0 ? 1 : -1;
+                    knots[knot].y += yd > 0 ? 1 : -1;    
+                }
+                else if (Math.abs(xd) > 1) {
+                    knots[knot].y = knots[knot - 1].y;
+                }
+                else if (Math.abs(yd) > 1) {
+                    knots[knot].x = knots[knot - 1].x;
+                }
+                */
+                if (moved) {
+                
+                    if (knot == 1) {
+                        console.log(`move(${move}): xd-${xd}, yd-${yd}, ${moves[move]}, knots[0]: ${JSON.stringify(knots[0])}`);
+                    }
+                    console.log(`move(${move}): xd-${xd}, yd-${yd}, ${moves[move]}, knots[${knot}]: ${JSON.stringify(knots[knot])}`);
                 }
 
                 if (moved && knot == 9) {
-                    console.log(`move(${move}): xd-${xd}, yd-${yd}, ${moves[move]}, knot[0]: ${JSON.stringify(knots[0])}}, knot[${knot}]:  ${JSON.stringify(knots[knot])}`)
-                    //let position = JSON.stringify({x : knots[9].x, y : knots[9].y});
-                    let position = JSON.stringify(knots[9]);
+                    i++;
+                
+                    let position = JSON.stringify({x : knots[knot].x, y : knots[knot].y});
                     if (!positions.has(position)) {
                         positions.add(position);
                     }
@@ -81,10 +98,17 @@ const { open } = require('node:fs/promises');
 
         }
 
-      
+        if (move == 100) {
+            //break;
+        }
         
     }
 
+    console.log(`i = ${i}`);
+
+    for (const val of positions) {
+        console.log(val);
+    }
     
     console.log(`positions: ${positions.size}`);   
 
