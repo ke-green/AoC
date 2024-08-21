dd0deaa38a6a867bad6538ddb215ee5686334242
ke-green <k.erikgreen@gmail.com>
2024-08-20 20:39:14 -0400

    Day 9

--- 2022/js/Day9/PartB.js
+++ 2022/js/Day9/PartB.js
@@ -5,7 +5,7 @@ const { open } = require('node:fs/promises');
 
 (async () => {
 
-    const file = await open('./SAMPLE2.TXT');
+    const file = await open('./INPUT.TXT');
     const moves = [];
     const positions = new Set();
 
@@ -13,10 +13,7 @@ const { open } = require('node:fs/promises');
         moves.push(move);    
     }
     
-    const knots = Array.from({ length: 10}, (_, i) => ({x : 11, y : 5}));
-    let tail = {x : 0, y : 0};
-    let i = 0;
-
+    const knots = Array.from({ length: 10}, (_, i) => ({x : 0, y : 0}));
    
     let position = JSON.stringify({x : knots[0].x, y : knots[0].y});
     positions.add(position);
@@ -43,17 +40,16 @@ const { open } = require('node:fs/promises');
                     break;
             }
 
-            
             for (let knot = 1; knot < 10; knot++) {
-            
+
                 let yd = (knots[knot - 1].y - knots[knot].y);
                 let xd = (knots[knot - 1].x - knots[knot].x);
                 let moved = false;
-
                 
                 if (Math.abs(xd) > 1 && Math.abs(yd) > 1) {
                     knots[knot].x += xd > 0 ? 1 : -1;
-                    knots[knot].y += yd > 0 ? 1 : -1;    
+                    knots[knot].y += yd > 0 ? 1 : -1;
+                    moved = true;    
                 }
                 else if (Math.abs(yd) > 1) {
                     knots[knot].y += yd > 0 ? 1 : -1;
@@ -66,29 +62,8 @@ const { open } = require('node:fs/promises');
                     moved = true;
                 } 
                 
-                /*
-                if (Math.abs(xd) > 1 && Math.abs(yd) > 1) {
-                    knots[knot].x += xd > 0 ? 1 : -1;
-                    knots[knot].y += yd > 0 ? 1 : -1;    
-                }
-                else if (Math.abs(xd) > 1) {
-                    knots[knot].y = knots[knot - 1].y;
-                }
-                else if (Math.abs(yd) > 1) {
-                    knots[knot].x = knots[knot - 1].x;
-                }
-                */
-                if (moved) {
-                
-                    if (knot == 1) {
-                        console.log(`move(${move}): xd-${xd}, yd-${yd}, ${moves[move]}, knots[0]: ${JSON.stringify(knots[0])}`);
-                    }
-                    console.log(`move(${move}): xd-${xd}, yd-${yd}, ${moves[move]}, knots[${knot}]: ${JSON.stringify(knots[knot])}`);
-                }
 
                 if (moved && knot == 9) {
-                    i++;
-                
                     let position = JSON.stringify({x : knots[knot].x, y : knots[knot].y});
                     if (!positions.has(position)) {
                         positions.add(position);
@@ -98,18 +73,8 @@ const { open } = require('node:fs/promises');
 
         }
 
-        if (move == 100) {
-            //break;
-        }
-        
     }
 
-    console.log(`i = ${i}`);
-
-    for (const val of positions) {
-        console.log(val);
-    }
-    
     console.log(`positions: ${positions.size}`);   
 
 
